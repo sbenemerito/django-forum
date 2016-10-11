8180880ff10a6ca72a03ba51db87fb88950b158f
sbenemerito <benemeritosam@gmail.com>
2016-10-12 00:26:38 +0800

    Edited settings.py

--- forums/settings.py
+++ forums/settings.py
@@ -141,5 +141,5 @@ if os.getcwd() == '/app':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'forum/static'),
     )
