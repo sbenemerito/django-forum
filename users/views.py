e31f835e6c7d22593676dd57aca7de6aa6912c50
sbenemerito <benemeritosam@gmail.com>
2016-10-20 22:05:42 +0800

    Added error messages in Register page (for testing)

--- users/views.py
+++ users/views.py
@@ -3,16 +3,21 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
-            login(request, authenticated_user)
+
+        if request.POST['username'] != '' or request.POST['password1'] != '' or request.POST['password2'] != '':
+            if form.is_valid():
+                new_user = form.save()
+                authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+                login(request, authenticated_user)
             return HttpResponseRedirect(reverse('forum:index'))
+        else:
+            messages.error(request, 'You left one or more field(s) blank')
+            
     else:
         form = UserCreationForm()
 
